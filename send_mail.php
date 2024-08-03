2e1ca7a5fdb25161d6d4b45548dbb32c7090409a
Natalia Moreira <NATII_MOREIRA@HOTMAIL.COM>
2024-08-03 14:11:36 -0300

    Agregado script PHP para enviar correo desde formulario de contacto

--- send_mail.php
+++ send_mail.php
@@ -1,12 +1,12 @@
 <?php
 // Verifica si el formulario ha sido enviado (método POST)
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recoger los datos del formulario
-    $nombre = $_POST['nombre'];
-    $telefono = $_POST['telefono'];
-    $correo = $_POST['correo'];
-    $tema = $_POST['tema'];
-    $mensaje = $_POST['mensaje'];
+    // Recoge los datos del formulario
+    $nombre = htmlspecialchars($_POST['nombre']);
+    $telefono = htmlspecialchars($_POST['telefono']);
+    $correo = htmlspecialchars($_POST['correo']);
+    $tema = htmlspecialchars($_POST['tema']);
+    $mensaje = htmlspecialchars($_POST['mensaje']);
 
     // Dirección de correo a la que se enviará el mensaje
     $destinatario = "natii_moreira@hotmail.com";
@@ -16,24 +16,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Construir el cuerpo del mensaje
     $contenido = "Nombre: $nombre\n";
-    $contenido .= "Teléfono: $telefono\n";
+    if (!empty($telefono)) {
+        $contenido .= "Teléfono: $telefono\n";
+    }
     $contenido .= "Correo: $correo\n";
-    $contenido .= "Tema: $tema\n\n";
+    if (!empty($tema)) {
+        $contenido .= "Tema: $tema\n\n";
+    }
     $contenido .= "Mensaje:\n$mensaje";
 
     // Cabeceras adicionales
-    $cabeceras = "From: $nombre <$correo>";
+    $cabeceras = "From: $nombre <$correo>\r\n";
+    $cabeceras .= "Reply-To: $correo\r\n";
 
     // Enviar el correo electrónico
-   
     if (mail($destinatario, $asunto, $contenido, $cabeceras)) {
         // Redirigir a una página de agradecimiento si se envía correctamente
         header('Location: gracias.html');
         exit;
     } else {
         // Mostrar un mensaje de error si hay un problema con el envío del correo
-        die("Hubo un problema al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.");
+        echo "Hubo un problema al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.";
     }
-    
 }
 ?>
+
